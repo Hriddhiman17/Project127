191959d027c5a5bb272e8cd5c745643d69f1b9f6
Hriddhiman <hriddhiman2008@gmail.com>
2021-08-08 17:34:37 +0530

    commit

---
+++ scrape.py
@@ -0,0 +1,19 @@
+import csv
+import time
+import random
+webLink = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+
+# time.sleep(10)
+
+def scrape():
+    headers = ["name", "distance", "mass", "radius"]
+    data = []
+    for i in random.randint(99, 999):
+        print(i)
+    with open("scrapper_2.csv", "w") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(headers)
+
+scrape()
+
+
